e12e15d634f97c4b4229aead63b4ee742befc3b9
cristian <cristian.s.aguero@icloud.com>
2023-08-04 11:39:23 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import Login from './components/Login'
 
 
 function App() {
 
   return (
- <div className=''>
+ <div className='bg-gray-400 w-full h-screen'>
      <BrowserRouter>
       <nav className='flex justify-around text-white items-center h-14 w-4/5 bg-[#343A40] rounded-md mb-12 '>
             <a href="/">login</a>
@@ -16,7 +16,7 @@ function App() {
       </nav>
       
       <Routes>
-          <Route path='/' element={<h1>login</h1>} />
+          <Route path='/' element={<Login />} />
           <Route path="/findlove" element={<h1>find love</h1>} />
           <Route path="/about" element={<h1>about</h1>} />
           <Route path="/succes" element={<h1>succes</h1>} />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,19 @@
 
 function Login() {
     return (
-        <div>Login</div>
+        <div className="flex justify-center">
+            <div className='flex justify-center flex-col  w-3/5'>
+            <h1 className="text-4xl text-white">Sign in</h1>
+            <form className="flex flex-col">
+                <label htmlFor="username">Name</label>
+                <input type="text" id="username" name="username" />
+                <label htmlFor="age">Age</label>
+                <input type="text" id="age" name="age" />
+                <label htmlFor="gender">Gender</label>
+                <input type='text' id='gender' name='gender' />
+            </form>
+        </div>
+        </div>
     )
 }
 
